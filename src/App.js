b4377f92cf8e0c96712b9590df48569aa2074f74
Sebastian Francisco Belmonte Cervero <sebastian.belmonte01@gmail.com>
2022-06-13 13:23:43 -0400

    Subiendo avance de frontend BMSC

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from './Pages/Main';
+import BancaDigital from './Pages/BancaDigital';
+import BancaFisica from './Pages/BancaFisica';
+import Movimientos from './Pages/Movimientos';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path ="/" element= {<Main />} />
+        <Route exact path ="/BancaDigital" element= {<BancaDigital />} />
+        <Route exact path ="/BancaFisica" element= {<BancaFisica />} />
+        <Route exact path ="/Movimientos" element= {<Movimientos />} />
+      </Routes>
+    </Router>
   );
 }
 
